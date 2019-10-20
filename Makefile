4c38904b06e2f85b501003853568144f7ef68c50
g-hyoga <hyoga0216@gmail.com>
2019-10-20 18:59:47 +0900

    Introduce Gauche

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
 .DEFAULT_GOAL := help
 
-pull-image: ## pull docker image
-	docker pull jackfirth/racket
-
 run: ## run register-machine
-	docker run -v "$$PWD/src:/src" -it jackfirth/racket racket ./src/main.rkt
+	docker run --rm -v $$PWD/src:/usr/src practicalscheme/gauche:latest gosh ./usr/src/main.scm
 
 help: ## show help to make
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- src/main.scm
+++ src/main.scm
@@ -1,7 +1,47 @@
-#lang racket
-(require "./list.rkt")
-(require "./register.rkt")
-(require "./stack.rkt")
+
+(define true #t)
+(define false #f)
+
+(define (make-register name)
+  (let ((contents '*unassigned*))
+    (define (dispatch message)
+      (cond ((eq? message 'get) contents)
+            ((eq? message 'set)
+             (lambda (value) (set! contents value)))
+            (else
+              (error "Unknown request: REGISTER"))))
+  dispatch))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+    (eq? (car exp) tag)
+    false))
+
+(define (make-stack)
+  (let((s '()))
+    (define (push x) (set! s (cons x s)))
+    (define (pop)
+      (if (null? s)
+          (error "Empty stack: POP")
+          (let ((top (car s)))
+            (set! s (cdr s))
+            top)))
+    (define (initialize)
+      (set! s '())
+      'done)
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+            ((eq? message 'pop) (pop))
+            ((eq? message 'initialize) (initialize))
+            (else (error "Unknown request: STACK" message))))
+    dispatch))
+
+(define (pop stack) (stack 'pop))
+(define (push stack value) ((stack 'push) value))
+
+(define (get-contents register) (register 'get))
+(define (set-contents! register value)
+  ((register 'set) value))
 
 (define (make-machine register-names ops controller-text)
   (let ((machine (make-new-machine)))
@@ -287,9 +327,8 @@
        (goto (label test-b))
        gcd-done)))
 
-; (set-register-contents! gcd-machine 'a 206)
-; (set-register-contents! gcd-machine 'b 40)
-; (start gcd-machine)
-; (get-register-contents gcd-machine 'a)
-
+(set-register-contents! gcd-machine 'a 206)
+(set-register-contents! gcd-machine 'b 40)
+(start gcd-machine)
+(print (get-register-contents gcd-machine 'a))
 
